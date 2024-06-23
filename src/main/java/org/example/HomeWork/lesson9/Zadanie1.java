77be7cce28ab7184c085991cb0b740cbd0b5700c
Enver <e.eredzhepov@goodt.me>
2024-06-23 09:42:56 +0300

    9 lesson HomeWork - done

---
+++ src/main/java/org/example/HomeWork/lesson9/Zadanie1.java
@@ -0,0 +1,20 @@
+package org.example.HomeWork.lesson9;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Zadanie1 z1 = new Zadanie1();
+        Zadanie1 z2 = new Zadanie1();
+        Zadanie1 z3 = new Zadanie1();
+        Zadanie1 z4 = new Zadanie1();
+        Zadanie1 z5 = new Zadanie1();
+        Zadanie1 z6 = new Zadanie1();
+        Zadanie1 z7 = new Zadanie1();
+        Zadanie1 z8 = new Zadanie1();
+        z1 = null;
+        z2 = null;
+        z3 = null;
+        z4 = null;
+        z5 = null;
+        z6 = null;
+    }
+}
