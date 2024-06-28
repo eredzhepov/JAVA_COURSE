2b8de5156b37b4e784b2f0807c622e888e0740b9
Enver <e.eredzhepov@goodt.me>
2024-06-28 22:20:56 +0300

    Revert "12 lesson HomeWork - done"
    This reverts commit 6933bf80b907bbab69e990f5b335424912b80c86.

--- src/main/java/org/example/HomeWork/lesson12/StudentTest.java
+++ src/main/java/org/example/HomeWork/lesson12/StudentTest.java
@@ -4,33 +4,30 @@ import org.example.lesson11.Student;
 
 public class StudentTest {
     public static void isStudentSame(Student s1, Student s2){
-        if(s1.name.equals(s2.name) && s1.course == s2.course && s1.garde == s2.garde) {
+        if(s1==s2){
             System.out.println("Студенты равны");
         } else {
             System.out.println("Студенты не равны");
         }
     }
-
-    public static void isStudentSameNest(Student s1, Student s2) {
-        if (s1.name.equals(s2.name)) {
-            if (s1.course == s2.course) {
-                if (s1.course == s2.course) {
-                    System.out.println("U studentov odinakovie imena, courses and grade");
-                } else {
-                    System.out.println("U studentov odinakovie imena, courses, no raznie ocenki");
-                }
-            } else {
-                System.out.println("U nich raznie Coursi");
-            }
+    public static void isStudentSameNest(Student s1, Student s2){
+        if(s1.name.equals(s2.name) && s1.course == s2.course && s1.garde == s2.garde){
+            System.out.println("Studenti odinakovie");
         } else {
-            System.out.println("U studentov raznie Imena");
-
+            System.out.println("Studenti ne ravni");
+            if(!s1.name.equals(s2.name) ){
+                System.out.println("U nich raznie Imena");
+            } else if (s1.course != s2.course) {
+                System.out.println("U nich raznie Coursi");
+            } else {
+                System.out.println("U nich raznie оценки");
+            }
         }
     }
 
     public static void main(String[] args) {
-        Student s1 = new Student("Igor", 2, 5.5);
-        Student s2 = new Student("Igor2", 2, 4.5);
+        Student s1 = new Student("Igor", 2, 4.5);
+        Student s2 = new Student("Igor", 2, 4.7);
         //isStudentSame(s1,s2);
         isStudentSameNest(s1,s2);
     }
