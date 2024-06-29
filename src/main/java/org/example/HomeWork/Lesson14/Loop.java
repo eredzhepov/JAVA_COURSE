0e2f10759ea012d7fd852b5a4b21d65f5eadfce0
Enver <e.eredzhepov@goodt.me>
2024-06-29 14:00:39 +0300

    14 lesson HomeWork - done

---
+++ src/main/java/org/example/HomeWork/Lesson14/Loop.java
@@ -0,0 +1,29 @@
+package org.example.HomeWork.Lesson14;
+
+public class Loop {
+    public static void showTime() {
+        HOURES:
+        for (int hour = 0; hour <= 6; hour++) {
+            MINUTES:
+            for (int minutes = 0; minutes <= 59; minutes++) {
+                if (hour > 1 && minutes % 10 == 0) {
+                    System.out.println("hour > 1");
+                    break HOURES;
+                }
+                SECONDS:
+                for (int seconds = 0; seconds <= 59; seconds++) {
+
+                    if (seconds * hour > minutes) {
+                        System.out.println("seconds*hour");
+                        continue MINUTES;
+                    }
+                    System.out.println(hour + " : " + minutes + " : " + seconds);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        showTime();
+    }
+}
---
+++ src/main/java/org/example/Lesson14/Test6.java
@@ -0,0 +1,17 @@
+package org.example.Lesson14;
+
+public class Test6 {
+    public void time() {
+       OUTER: for(int chas = 0; chas <= 23; chas++){
+            INNER: for(int minuta = 0; minuta <=59; minuta++){
+                System.out.println(chas + " : " + minuta);
+                if(minuta==30) break OUTER;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Test6 t = new Test6();
+        t.time();
+    }
+}
